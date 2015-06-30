ac3fcedfe06cf5f8c88639155a41ce8517513941
James Johnstone <james.johnstone@geeks.ltd.uk>
2015-06-30 13:47:53 +0100

    added initial entity structure

---
+++ SaguModel/Entities/Area.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaguModel
+{
+    public class Area
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+    }
+}
---
+++ SaguModel/Entities/ExploredArea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaguModel
+{
+    public class ExploredArea
+    {
+        public Guid ID { get; set; }
+        public Guid ExplorerId { get; set; }
+        public Explorer Explorer { get; set; }
+        public Guid AreaId { get; set; }
+        public Area Area { get; set; }
+        public double AmountExplored { get; set; }
+    }
+}
