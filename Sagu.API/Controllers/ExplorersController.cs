5d4b7064b210cd9ffd9a24fbdc47c621dfb3acab
James Johnstone <james.johnstone@geeks.ltd.uk>
2015-07-01 11:10:44 +0100

    Fix typo in Explorers controller, Add Get action

---
+++ Sagu.API/Controllers/ExplorersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Sagu.Services;
+
+namespace Sagu.API.Controllers
+{
+    public class ExplorersController : ApiController
+    {
+        ExplorerService ExplorerService = new ExplorerService();
+
+        public IHttpActionResult Get()
+        {
+            var explorers = ExplorerService.GetExplorers();
+
+            return Ok(explorers);
+        }
+    }
+}
