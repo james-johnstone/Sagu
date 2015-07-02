c5c477d19561c6dd20cc55a97e9232513db676e7
James Johnstone <james.johnstone@geeks.ltd.uk>
2015-07-02 08:44:26 +0100

    Add get and post methods to Explorer Controller

--- Sagu.API/Controllers/ExplorersController.cs
+++ Sagu.API/Controllers/ExplorersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Sagu.Model;
 using Sagu.Services;
 
 namespace Sagu.API
@@ -17,6 +18,33 @@ namespace Sagu.API
             var explorers = ExplorerService.GetExplorers();
 
             return Ok(explorers);
+        }
+
+        public IHttpActionResult Get(Guid id)
+        {
+            var explorer = ExplorerService.GetExplorer(id);
+
+            if (explorer == null)
+                return NotFound();
+
+            return Ok(explorer);
+        }
+
+        public IHttpActionResult Post([FromBody] Explorer explorer)
+        {
+            if (explorer == null)
+                return BadRequest();
+
+            try
+            {
+                var newExplorer = ExplorerService.CreateExplorer(explorer);
+
+                return Created(string.Format("{0}/{1}", Request.RequestUri, newExplorer.Id), newExplorer);
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
         }
     }
 }
